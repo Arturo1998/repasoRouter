24275bab329dd73ae1c1e3db5350cac9a81caa07
Arturo1998 <amartinvaras@gmail.com>
2023-01-30 13:35:42 +0100

    primer

---
+++ vueClaseSpa/src/views/About.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Sobre Nosotros</h1>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ vueClaseSpa/src/views/ListadoPers.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Lista Personajes Favoritos</h1>
+  </div>
+  <div>
+    <h2>Iron Man</h2>
+    <h2>Spider Man</h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
